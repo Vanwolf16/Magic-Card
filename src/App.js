66803c15bab76a47e7c37d32e91b31ec1aa619d7
vanwolf16 <d.murillo28@yahoo.com>
2023-06-02 14:12:47 -0700

    matched the cards added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import './App.css';
 import SingleCard from './components/SingleCard';
 
 
 const cardImages = [
-  {"src": "/img/helmet-1.png"},
-  {"src": "/img/potion-1.png"},
-  {"src": "/img/ring-1.png"},
-  {"src": "/img/scroll-1.png"},
-  {"src": "/img/shield-1.png"},
-  {"src": "/img/sword-1.png"}
+  {"src": "/img/helmet-1.png", matched: false},
+  {"src": "/img/potion-1.png", matched: false},
+  {"src": "/img/ring-1.png", matched: false},
+  {"src": "/img/scroll-1.png", matched: false},
+  {"src": "/img/shield-1.png", matched: false},
+  {"src": "/img/sword-1.png", matched: false}
 ]
 
 function App() {
@@ -36,7 +36,36 @@ function App() {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
   }
 
-  console.log(cards, turns)
+  //compare 2 selected cards
+  useEffect(() => {
+    if(choiceOne && choiceTwo){
+      if(choiceOne.src === choiceTwo.src){
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === choiceOne.src){
+              return {...card, matched: true}
+            }else{
+              return card
+            }
+          })
+        })
+        resetTurn()
+      }else{
+        
+        resetTurn()
+      }
+    }
+  }, [choiceOne,choiceTwo])
+
+  //reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
+
+  //console.log(cards, turns)
+  console.log(cards)
 
   return (
     <div className="App">
