0ee117f78f75b16a232c5f9b11a51a35a0db18a4
vanwolf16 <d.murillo28@yahoo.com>
2023-06-02 13:47:46 -0700

    making a card choice

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import { useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
+
 
 const cardImages = [
   {"src": "/img/helmet-1.png"},
   {"src": "/img/potion-1.png"},
   {"src": "/img/ring-1.png"},
   {"src": "/img/scroll-1.png"},
-  {"src": "/img/sheild-1.png"},
+  {"src": "/img/shield-1.png"},
   {"src": "/img/sword-1.png"}
 ]
 
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+
+  //choice states
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   //shuffle cards
   const shuffleCards = () => {
@@ -25,12 +31,28 @@ function App() {
     setTurns(0)
   }
 
+  //handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
   console.log(cards, turns)
 
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard key={card.id} 
+          card={card}
+          handleChoice = {handleChoice}
+          />
+        ))}
+
+      </div>
+
     </div>
   );
 }
